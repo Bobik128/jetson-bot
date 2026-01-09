936d828094a40a6ad728dd95e345f059569a0172
tomas <bocan.tomas.6666@gmail.com>
2026-01-09 14:55:45 +0100

    alll

--- src/feetech/follower_receive_u01.py
+++ src/feetech/follower_receive_u01.py
@@ -143,9 +143,9 @@ def is_touching_danger_zone(a, b, c, r) -> bool:
     x1 = math.cos(a) * 11.6
     y1 = math.sin(a) * 11.6
 
-    omega = math.pi - a - b
+    omega = -(math.pi - a - b)
     x2 = math.cos(omega) * 10.5
-    y2 = -math.sin(omega) * 10.5
+    y2 = math.sin(omega) * 10.5
 
     fi = omega + (c - math.pi)
     x3 = math.cos(fi) * 5.7
