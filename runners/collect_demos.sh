ef8de4f753c31c2dfaefbf9670e4ba1e74fdc632
tomas <bocan.tomas.6666@gmail.com>
2026-01-12 15:36:47 +0100

    runner update

--- runners/collect_demos.sh
+++ runners/collect_demos.sh
@@ -1,1 +1,7 @@
-/usr/bin/python3 ../src/collect_demos.py --preview --preview-height 540 --preview-width 920
+/usr/bin/python3 ../src/collect_demos.py \
+  --preview \
+  --preview-height 540 \
+  --preview-width 920 \
+  --arm-enable \
+  --arm-udp-ip 127.0.0.1 \
+  --arm-udp-port 5005
