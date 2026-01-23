9c1a2a10d82db7b646e9fe90e36e4d407bd40459
tomas <bocan.tomas.6666@gmail.com>
2026-01-23 15:07:45 +0100

    ahh

--- src/lerobot_robot_jetsonbot/lerobot_robot_jetsonbot/__init__.py
+++ src/lerobot_robot_jetsonbot/lerobot_robot_jetsonbot/__init__.py
@@ -2,6 +2,7 @@
 from .config_jetsonbot import JetsonBotClientConfig, JetsonBotConfig
 from .jetsonbot import JetsonBot
 from .jetsonbot_client import JetsonBotClient
+from .shared.esp32_link import ESP32Link
 
 # Optional: do not import hardware-dependent modules at package import time
 # Users can import these explicitly when needed.
