c6fd9c1398c11ec2d36e608c68a8dd55f28ca959
tomas <bocan.tomas.6666@gmail.com>
2026-04-09 20:35:44 +0200

    changed init.py

--- src/lerobot_teleoperator_dualsense/lerobot_teleoperator_dualsense/__init__.py
+++ src/lerobot_teleoperator_dualsense/lerobot_teleoperator_dualsense/__init__.py
@@ -1,17 +1,18 @@
-from .combo_teleop import SOLeaderPlusDualsense
-from .config_teleop_combo import SOLeaderPlusDualsenseConfig
-from .config_teleop import DualsenseTeleopConfig
-from .mapped_teleop import MappedTeleop
-from .dualsense_listener import DualsenseEpisodeButtons, DualsenseEpisodeListener
+# from .combo_teleop import SOLeaderPlusDualsense
+# from .config_teleop_combo import SOLeaderPlusDualsenseConfig
+# from .config_teleop import DualsenseTeleopConfig
+# from .mapped_teleop import MappedTeleop
+# from .dualsense_listener import DualsenseEpisodeButtons, DualsenseEpisodeListener
 
-__all__ = [
-    "DualsenseTeleop",
-    "DualsenseTeleopConfig",
-    "SOLeader",
-    "SOLeaderTeleopConfig",
-    "SOLeaderPlusDualsense",
-    "SOLeaderPlusDualsenseConfig",
-    "MappedTeleop",
-    "DualsenseEpisodeButtons",
-    "DualsenseEpisodeListener",
-]
+# __all__ = [
+#     "DualsenseTeleop",
+#     "DualsenseTeleopConfig",
+#     "SOLeader",
+#     "SOLeaderTeleopConfig",
+#     "SOLeaderPlusDualsense",
+#     "SOLeaderPlusDualsenseConfig",
+#     "MappedTeleop",
+#     "DualsenseEpisodeButtons",
+#     "DualsenseEpisodeListener",
+# ]
+__all__ = []
