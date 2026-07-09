cb343ea55b2b759e3397fbc8559df04c2a5d572d
tomas <bocan.tomas.6666@gmail.com>
2026-07-09 11:57:54 +0200

    noteleop update

---
+++ runners/run_evaluate_jetson_noteleop.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+python ./../src/scripts/evaluate.py \
+  --policy act \
+  --where jetson \
+  --teleop none \
+  --remote-ip 127.0.0.1 \
+  --robot-id jetson-bot \
+  --hf-model-id Bobik553/jetson-bot_policy-blue_cubes_in_red-NEO-1 \
+  --hf-eval-dataset-base-id Bobik553/jetson-bot_blue-block-on-box_eval-help-NEO \
+  "$@"
